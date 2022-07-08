ffcaa8cded1fd3093af867345a538663c5267af6
Paul Vatterott <pfvatterott@gmail.com>
2022-07-08 17:03:10 -0600

    initial build

---
+++ app.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const express = require("express")
+const bodyParser = require("body-parser");
+const app = express()
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+app.use(bodyParser.json())
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+//webhook listener and validator
+app.get("/hook", (req, res) => {
+    if (req.query[challenge]) {
+        console.log(req.query[challenge])
+        let ChallengeId = req.query[challenge]
+        res.status(200).send(
+            {
+                'challenge': ChallengeId
+            }
+        )
+    }
+    else {
+        res.status(200).end()
+    }
+})
